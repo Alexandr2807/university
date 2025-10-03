02abfbbbbfa237504a09adf434272a9b5607ba63
Alexandr2807 <121241858+Alexandr2807@users.noreply.github.com>
2025-10-03 14:55:13 +0500

    Add files via upload

---
+++ chesstable.cs
@@ -0,0 +1,15 @@
+﻿static void WriteBoard(int size)
+{
+    for (int i = 0; i < size; i++)        // строки
+    {
+        for (int j = 0; j < size; j++)    // столбцы
+        {
+            if ((i + j) % 2 != 0)
+                Console.Write('.');
+            else
+                Console.Write('#');
+        }
+        Console.WriteLine(); // переход на новую строку
+    }
+    Console.WriteLine();
+}
